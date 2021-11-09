443c22c41a6f4009c393d45926efd56c67d2fe10
Yajnesh Prasad Mohanty <48801350+yajnesh99@users.noreply.github.com>
2021-11-09 21:33:03 +0530

    Update full_code.py

--- chatbot_codes/full_code.py
+++ chatbot_codes/full_code.py
@@ -131,7 +131,7 @@ def get_response(return_list,intents_json):
 
     if tag=='google':
         query=input('Enter query...')
-        chrome_path = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
+        #chrome_path = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
         for url in search(query, tld="co.in", num=1, stop = 1, pause = 2):
             webbrowser.open("https://google.com/search?q=%s" % query)
     if tag=='weather':
